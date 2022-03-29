7459f6a85aa5bdd143b756dcdb60cb126f09c515
nicolasabosch <nicolasabosch@gmail.com>
2022-03-29 15:44:38 -0300

    ejIMC WORKING

--- Assets/ejIMC.cs
+++ Assets/ejIMC.cs
@@ -14,7 +14,27 @@ public class ejIMC : MonoBehaviour
 
        case int imc when (imc >= 18.5 && imc <=24.9):
 
-       Debug.Log ("");
+       Debug.Log ("Normal");
+       break;
+
+       case int imc when (imc >= 25 && imc <=29.9):
+
+       Debug.Log ("Sobrepeso");
+       break;
+
+       case int imc when (imc >= 30 && imc <=34.9):
+
+       Debug.Log ("Obesidad grado 1");
+       break;
+
+       case int imc when (imc >= 35 && imc <=39.9):
+
+       Debug.Log ("Obesidad grado 2");
+       break;
+
+       case int imc when (imc >= 40):
+
+       Debug.Log ("Obesidad grado 3");
        break;
        }
     }
